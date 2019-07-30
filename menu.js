76ee78b37c50b0f9a937d65e4a5363857d323340
david.khudaverdyan <d.khudaverdyan@plaxis.com>
2019-07-30 16:27:12 +0200

    fix from menu.js

--- menu.js
+++ menu.js
@@ -3,11 +3,11 @@
     var string = '<div align ="center"><font color="red" size="6"> <b> Hovhannes M. Khudaverdian </b></font></div>';
     string += '<p align="center">';
     string += '  <small>';
-    string += '    <a href="http://khudian.net/index.html" target="main">Main page</a>';
-    string += '    <a href="http://khudian.net/Research/temp.html" target="main">Research</a>';
-    string += '    <a href="http://khudian.net/Teaching/teaching.html" target="main">Teaching</a>';
-    string += '    <a href="http://khudian.net/Lectures/lectures.html" target="main">Lectures</a>';
-    string += '    <a href="http://khudian.net/Etudes/etudes.html" target="main">&Eacute;tudes</a>';
+    string += '    <a href="http://khudian.net/index.html">Main page</a>';
+    string += '    <a href="http://khudian.net/Research/temp.html">Research</a>';
+    string += '    <a href="http://khudian.net/Teaching/teaching.html">Teaching</a>';
+    string += '    <a href="http://khudian.net/Lectures/lectures.html">Lectures</a>';
+    string += '    <a href="http://khudian.net/Etudes/etudes.html">&Eacute;tudes</a>';
     string += '  </small>';
     string += '</p>';
     string += '<hr>';
