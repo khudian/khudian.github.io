bef43a009e93b102d75cd7b16d89d8d511987620
David <khdavid@mail.ru>
2019-08-27 12:03:04 +0300

    improve style

--- experiment/libs/mathBlogArticle.js
+++ experiment/libs/mathBlogArticle.js
@@ -9,8 +9,11 @@ function addLinkToMathBlog()
   html = `<div class="mathBlogButton">
     <a class="mathBlogButton" href="../../../index.html">Math Blog</a>
   </div>`
+  $("div.mainText").prepend('<br><br>');
   $("div.mainText").prepend(html);
+  $("div.mainText").append('<br>');
   $("div.mainText").append(html);
+  $("div.mainText").append('<br><br>');
 }
 
 $( document ).ready(function() 
