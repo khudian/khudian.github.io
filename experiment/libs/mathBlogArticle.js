b45838f54e2769d300c01d02445b102457371045
David <khdavid@mail.ru>
2019-08-27 11:47:25 +0300

    improve links

--- experiment/libs/mathBlogArticle.js
+++ experiment/libs/mathBlogArticle.js
@@ -4,9 +4,19 @@ function propogateDate()
   $('div.article').prepend(html);
 }
 
+function addLinkToMathBlog()
+{
+  html = `<div class="mathBlogButton">
+    <a class="mathBlogButton" href="../../../index.html">Math Blog</a>
+  </div>`
+  $("div.mainText").prepend(html);
+  $("div.mainText").append(html);
+}
+
 $( document ).ready(function() 
 { 
   propogateDate();
   linkFootnotes();
-}
+  addLinkToMathBlog();
+  }
 );
