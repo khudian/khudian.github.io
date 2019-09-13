5852d3a3feaba6e74aa58aa75c565ebd04a3306d
david.khudaverdyan <d.khudaverdyan@plaxis.com>
2019-09-13 15:53:38 +0200

    move Experiment to MathBlog folder

--- menu.js
+++ menu.js
@@ -4,6 +4,7 @@
     string += '<p align="center">';
     string += '  <small>';
     string += '    <a href="http://khudian.net/index.html">Main page</a>';
+    string += '    <a href="http://khudian.net/MathBlog/index.html">Math Blog</a>';
     string += '    <a href="http://khudian.net/Research/temp.html">Research</a>';
     string += '    <a href="http://khudian.net/Teaching/teaching.html">Teaching</a>';
     string += '    <a href="http://khudian.net/Lectures/lectures.html">Lectures</a>';
