13c1dc50ad7b171f76e00a81434ad8d7ee2619a4
David <khdavid@mail.ru>
2020-02-12 22:07:31 +0100

    improve converter tool

--- MathBlog/libs/python/converterTool.py
+++ MathBlog/libs/python/converterTool.py
@@ -6,11 +6,40 @@ gDestinationDir = R"C:\Users\khuda\Desktop\khudian.github.io\MathBlog"
 
 print(gBlogDir)
 
+def monthStringToNumber(monthString):
+  monthsMap = {'jan': 1,
+    'feb': 2,
+    'mar': 3,
+    'apr':4,
+    'may':5,
+    'jun':6,
+    'jul':7,
+    'aug':8,
+    'sep':9,
+    'oct':10,
+    'nov':11,
+    'dec':12}
+  
+  strippedString = monthString.strip()[:3].lower()
+  try:
+    out = monthsMap[strippedString]
+    return out
+  except:
+    raise ValueError('Not a month')
+        
+        
 def getMonthAndYear(subdir):
   splitted = os.path.split(subdir)
   month = splitted[1]
   year = os.path.split(splitted[0])[1]
   return [month, year]
+
+def getMonthAndYearNum(month, year):
+  return [monthStringToNumber(month), int(year)]
+
+  
+def validateMonthAndYear(month, year):
+  return (year == 2019) and (month > 6) or (year > 2019) 
 
 for subdir, dirs, files in os.walk(gBlogDir):
   for file in files:
@@ -19,7 +48,9 @@ for subdir, dirs, files in os.walk(gBlogDir):
       [month, year] = getMonthAndYear(subdir)
       targetDir = os.path.join(gDestinationDir, year, month, Path(file).stem)
       targetFile = os.path.join(targetDir, 'index.html')
-      if not os.path.exists(targetFile):
+      [monthNum, yearNum] = getMonthAndYearNum(month, year)
+      if (not os.path.exists(targetFile) and 
+        validateMonthAndYear(monthNum, yearNum)):
         print(targetFile)
         
       
