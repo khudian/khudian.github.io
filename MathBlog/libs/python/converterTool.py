d0de010d884c38fd4853161478c0349b16505222
David <khdavid@mail.ru>
2020-02-16 14:10:55 +0100

    improve log

--- MathBlog/libs/python/converterTool.py
+++ MathBlog/libs/python/converterTool.py
@@ -4,9 +4,37 @@ from shutil import copyfile
 
 gBlogDir = R"C:\Users\khuda\Desktop\Blog"
 gDestinationDir = R"C:\Users\khuda\Desktop\khudian.github.io\MathBlog"
+gTemplate = """
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <link rel="stylesheet" type="text/css" href="../../../libs/mathBlogStyle.css">
 
-print(gBlogDir)
+  <script type="text/javascript" src="../../../libs/jquery_3.4.1.js"></script>
+  <script type="text/javascript" src="../../../libs/menu.js"></script>
+  <script type="text/javascript" src="../../../config.js"></script> 
+  <script type="text/javascript" src="../../../libs/mathBlogBase.js"></script>
+  <script type="text/javascript" src="../../../libs/mathBlogArticle.js"></script> 
+  <script type="text/x-mathjax-config">
+    configureMathJax();
+  </script>
+  <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML"></script>  
+</head>
 
+<body>
+  <div id="menu"></div>
+
+  <div class="mainText">
+    <div class="article" date="PYTHON_DATE_KEY">
+      PYTHON_MAIN_ARTICLE_KEY
+    </div>
+ </body>
+</html>
+"""
+
+print(gTemplate)
 def monthStringToNumber(monthString):
   monthsMap = {'jan': 1,
     'feb': 2,
@@ -42,23 +70,43 @@ def getMonthAndYearNum(month, year):
 def validateMonthAndYear(month, year):
   return (year == 2019) and (month > 6) or (year > 2019) 
 
+def cleverCopy(inFile, targetFile):
+  targetFolder = os.path.split(targetFile)[0];
+  if not os.path.exists(targetFolder):
+    os.makedirs(targetFolder)
+  copyfile(inFile, targetFile)  
+  
 def convert(inFile, targetFile):
   print("converting: " + inFile)
   print("destination:" + targetFile)
-  #copyfile(inFile, targetFile)
-  #copyfile(inFile, targetFile)
+  cleverCopy(inFile, targetFile)
   print("")
 
-for subdir, dirs, files in os.walk(gBlogDir):
-  for file in files:
-    if file.endswith("tex"):
-      fullPath = os.path.join(subdir, file)
-      [month, year] = getMonthAndYear(subdir)
-      targetDir = os.path.join(gDestinationDir, year, month, Path(file).stem)
-      targetFile = os.path.join(targetDir, 'index.html')
-      [monthNum, yearNum] = getMonthAndYearNum(month, year)
-      if (not os.path.exists(targetFile) and 
-        validateMonthAndYear(monthNum, yearNum)):
-        convert(fullPath, targetFile)
-        
-      
+def convert(pathFrom, pathTo):
+  with open(pathFrom, 'r') as file:
+    data = file.read();
+
+  result = gTemplate.replace("PYTHON_DATE_KEY", "SDFDSF")
+  result = result.replace("PYTHON_MAIN_ARTICLE_KEY", data)
+    
+  with open(pathTo, 'w') as file:
+    file.write(result)  
+    
+def execute():
+  for subdir, dirs, files in os.walk(gBlogDir):
+    for file in files:
+      if file.endswith("tex"):
+        fullPath = os.path.join(subdir, file)
+        [month, year] = getMonthAndYear(subdir)
+        targetDir = os.path.join(gDestinationDir, year, month, Path(file).stem)
+        targetFile = os.path.join(targetDir, 'index.html')
+        [monthNum, yearNum] = getMonthAndYearNum(month, year)
+        if (not os.path.exists(targetFile) and 
+          validateMonthAndYear(monthNum, yearNum)):
+          convert(fullPath, targetFile)
+
+execute();
+convert(R"C:\Users\khuda\Desktop\khudian.github.io\MathBlog\2020\January20\9theorem\index.html",
+R"C:\Users\khuda\Desktop\khudian.github.io\MathBlog\2020\January20\9theorem\index_covnertes.html")
+          
+
